b32245848dff3cf5a86bad706c9056020e376f12
wfhopkins <will.hop@gmail.com>
2023-05-29 18:22:30 -0700

    created the assertEquals function and added emoji

---
+++ assertEqual.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("❌❌❌Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("cats", "cats");
+assertEqual("cats", "dogs");
+assertEqual(7, 7);
+assertEqual(4, 11);
+
