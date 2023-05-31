c3c5f9079f5fa79d371a2bd0e07aff7f2c6a5f5f
wfhopkins <will.hop@gmail.com>
2023-05-30 19:10:25 -0700

    implemented assertArraysEqual function combining the eqArrays and assertEqual functions

---
+++ assertArraysEqual.js
@@ -0,0 +1,26 @@
+let eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+let assertArraysEqual = function(arr1, arr2) {
+  let result = eqArrays(arr1, arr2);
+  if (result === true) {
+    console.log("Arrays are equal:", arr1, arr2)
+  } else {  
+    console.log("Arrays are not equal:", arr1, arr2)
+  }
+  return eqArrays(arr1, arr2);
+}
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [1, 2, 4]);
+
+assertArraysEqual([1, 2, "3"], [1, 2, 3]);
+assertArraysEqual(["dog", "cat", "fish"], ["dog", "cat", "fish"]);
+assertArraysEqual([1, -2, 3], [1, 2, 3]);
+assertArraysEqual([1, -2, 4], [1, -2, 4]);
