0dfd317b2d3e3238f7a7de3a74911efc2ec6f003
wfhopkins <will.hop@gmail.com>
2023-05-30 21:23:19 -0700

    implementing flatten function to manage arrays with arrays nested inside

---
+++ flatten.js
@@ -0,0 +1,32 @@
+let assertArraysEqual = function(arr1, arr2) {
+  let result = eqArrays(arr1, arr2);
+  if (result === true) {
+    console.log("Arrays are equal:", arr1, arr2);
+  } else {
+    console.log("Arrays are not equal:", arr1, arr2);
+  }
+  return eqArrays(arr1, arr2);
+};
+
+let eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const flatten = function(arrays) {
+  let newArray = [];
+  for (let i = 0; i < arrays.length; i++) {
+    if (Array.isArray(arrays[i])) {
+      newArray = newArray.concat(flatten(arrays[i]));
+    } else {
+      newArray.push(arrays[i]);
+    }
+  }
+  return newArray;
+};
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
