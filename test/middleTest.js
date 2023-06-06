fb549cbb8a53619f42ceafe626588adbe2207ae3
wfhopkins <will.hop@gmail.com>
2023-06-05 18:20:37 -0700

    updated middle and tail test files for mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,25 +1,25 @@
+const assert = require("chai").assert;
 const middle = require("../middle")
-const assertArraysEqual = require("../assertArraysEqual")
 
 describe("middle", function() {
   it("should print nothing when array is less than 3", function() {
     const arr1 = [1];
     const actual = middle(arr1);
     const expected = [];
-    assertArraysEqual(actual, expected)
+    assert.deepEqual(actual, expected)
   });
 
   it("should print middle number when array is odd", function() {
     const arr1 = [1, 2, 3, 4, 5];
     const actual = middle(arr1);
     const expected = [3];
-    assertArraysEqual(actual, expected)
+    assert.deepEqual(actual, expected)
   });
 
   it("should print both numbers at the middle when array is even", function() {
     const arr1 = [2, 4, 6, 8, 10, 12];
     const actual = middle(arr1);
     const expected = [6, 8];
-    assertArraysEqual(actual, expected)
+    assert.deepEqual(actual, expected)
   });
 });
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,18 +1,18 @@
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require("chai").assert;
 const tail = require("../tail.js");
 
 describe("tail", function() {
-  it("should return all but the first item in an array when given an array", function() {
+  it("should return (7, 2, 9) when given (3, 7, 2, 9)", function() {
     const arr1 = [3, 7, 2, 9];
     const actual = tail(arr1);
     const expected = [7, 2, 9];
-    assertArraysEqual(actual, expected)
+    assert.deepEqual(actual, expected)
   });
 
-  it("should return all but the first item in an array when given an array", function() {
+  it("should return the (Doody, Dandy) when given (Howdy, Doody, Dandy)", function() {
     const arr2 = ["Howdy", "Doody", "Dandy"];
     const actual = tail(arr2);
     const expected = ["Doody", "Dandy"];
-    assertArraysEqual(actual, expected)
+    assert.deepEqual(actual, expected)
   });
 });
