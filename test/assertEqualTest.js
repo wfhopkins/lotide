6e5a675a2eee1fbf94de320f21cdae5d2f2ce36b
wfhopkins <will.hop@gmail.com>
2023-06-05 18:08:42 -0700

    updated headTest to use mocha/chai, added .gitignore file to handle node_modules

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,17 +1,17 @@
 const assertEqual = require("../assertEqual");
 
 describe("assertEqual", function() {
-  it("should return passing or failing depending on actual vs expected output"), function() {
-    const arr1 = [3, 7, 2, 9];
-    const actual = tail(arr1);
-    const expected = [7, 2, 9];
+  it("should return failing if actual and expected are NOT equal ", function() {
+    const data = "orses"
+    const actual = data;
+    const expected = "horses";
     assertEqual(actual, expected)
-  }
+  });
 
-  it("should return passing or failing depending on actual vs expected output"), function() {
-    const arr1 = [3, 7, 2, 9];
-    const actual = tail(arr1);
-    const expected = [7, 2, 9];
+  it("should return passing passing if actual and expected are equal", function() {
+    const data = (3, 7, 2, 9);
+    const actual = data;
+    const expected = (3, 7, 2, 9);
     assertEqual(actual, expected)
-  }
+  });
 });
--- test/headTest.js
+++ test/headTest.js
@@ -1,18 +1,25 @@
-const assertArraysEqual = require("../assertArraysEqual")
+const assert = require("chai").assert;
 const head = require("../head.js");
 
-describe("head", function() {
-  it("should return the first item in a given array", function() {
+describe("head", () => {
+  it("should return the first item in a given array", () => {
     const arr1 = [3, 7, 2, 9];
     const actual = head(arr1);
-    const expected = [3];
-    assertArraysEqual(actual, expected)
-  })
+    const expected = 3;
+    assert.strictEqual(actual, expected)
+  });
 
-  it("should return the first item in a given array", function() {
-    const arr2 =["It's", "A", "Wonderful", "Life"];
+  it("returns a single character for an array with one character", () => {
+    const arr1 = [5];
+    const actual = head(arr1);
+    const expected = 5;
+    assert.strictEqual(actual, expected)
+  });
+
+  it("should return the first item in a given array", () => {
+    const arr2 =["It\'s", "A", "Wonderful", "Life"];
     const actual = head(arr2);
-    const expected = ["It's"];
-    assertArraysEqual(actual, expected)
-  })
+    const expected = "It\'s";
+    assert.strictEqual(actual, expected)
+  });
 });
